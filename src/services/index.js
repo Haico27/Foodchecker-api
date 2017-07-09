1e7a8604887438d154ef6f76565cac8f9dcabce5
Haico27 <haico.wensink@gmail.com>
2017-07-09 17:34:04 +0200

    generated authentication service, with user service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const fooditems = require('./fooditems/fooditems.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(fooditems);
+  app.configure(users);
 };
