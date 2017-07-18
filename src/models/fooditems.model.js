2ead2459235f1df38ab46bce70d1583e9bc596ec
Haico27 <haico.wensink@gmail.com>
2017-07-18 22:08:49 +0200

    populate author and restrict find- and create-methods to owner

--- src/models/fooditems.model.js
+++ src/models/fooditems.model.js
@@ -4,11 +4,15 @@
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
+
+  const { Schema } = mongooseClient;
+  
   const fooditems = new mongooseClient.Schema({
     title: { type: String, required: true },
     picture: { type: String, required: true },
     ingredients: { type: String, required: true },
     tolerated: { type: Boolean, required: true, 'default' : false },
+    authorId: { type: Schema.Types.ObjectId, ref: 'users' },
 
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
--- src/services/fooditems/fooditems.hooks.js
+++ src/services/fooditems/fooditems.hooks.js
@@ -1,18 +1,47 @@
+const { authenticate } = require('feathers-authentication').hooks;
+const { restrictToOwner, associateCurrentUser, restrictToAuthenticated } = require('feathers-authentication-hooks');
+const { populate } = require('feathers-hooks-common');
 
+
+
+const restrict = [
+  authenticate('jwt'),
+  restrictToOwner({
+    ownerField: 'authorId'
+  })
+];
+
+const authorSchema = {
+  include: {
+    service: 'users',
+    nameAs: 'author',
+    parentField: 'authorId',
+    childField: '_id'
+  }
+};
 
 module.exports = {
   before: {
     all: [],
-    find: [],
-    get: [],
-    create: [],
+    find: [
+      ...restrict
+    ],
+    get: [
+    ],
+    create: [
+      authenticate('jwt'),
+      restrictToAuthenticated(),
+      associateCurrentUser({ as: 'authorId' }),
+    ],
     update: [],
     patch: [],
     remove: []
   },
 
   after: {
-    all: [],
+    all: [
+      populate({ schema: authorSchema })
+    ],
     find: [],
     get: [],
     create: [],
